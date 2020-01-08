226a8580b1104e66833b364d20c46392fc05fa6f
Toraja <toraja.blend.french@gmail.com>
2020-01-09 08:45:21 +0900

    Replaced time.sleep() with better wait handling

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,7 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT_SEC = 10
+INTERVAL_SEC = 0.5
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,6 +16,24 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         input('Press Enter to continue...')
         self.browser.quit()
+
+    def run_func_until_time_has_passed(self, max_wait_sec, interval_sec, func,
+                                       arg_list):
+        start_time = time.time()
+
+        def time_has_passed():
+            if time.time() - start_time > max_wait_sec:
+                return True
+            return False
+
+        while True:
+            try:
+                func(*arg_list)
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time_has_passed():
+                    raise e
+                time.sleep(interval_sec)
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
@@ -40,9 +62,9 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-to list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.run_func_until_time_has_passed(MAX_WAIT_SEC, INTERVAL_SEC,
+                                            self.check_for_row_in_list_table,
+                                            ['1:Buy peacock feathers'])
 
         # There is still a text box invinting her to add another item.
         # She enters "Use peacock feathers to make a fly"
@@ -50,12 +72,14 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, now shows both items on her list
-        self.check_for_row_in_list_table('1:Buy peacock feathers')
-        self.check_for_row_in_list_table(
-            '2:Use peacock feathers to make a fly')
+        self.run_func_until_time_has_passed(MAX_WAIT_SEC, INTERVAL_SEC,
+                                            self.check_for_row_in_list_table,
+                                            ['1:Buy peacock feathers'])
+        self.run_func_until_time_has_passed(
+            MAX_WAIT_SEC, INTERVAL_SEC, self.check_for_row_in_list_table,
+            ['2:Use peacock feathers to make a fly'])
 
         # Edith wonder whether the site will remember the list.
         # Then she sees that the site has generated a unique URL for her --
