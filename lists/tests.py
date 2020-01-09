b82142b87568a4f058e3a0783c8804ef163a21f3
Toraja <toraja.blend.french@gmail.com>
2020-01-09 16:46:23 +0900

    Added UT: to-do list should have its own view

--- lists/tests.py
+++ lists/tests.py
@@ -32,8 +32,19 @@ class HomePageTest(TestCase):
 
         response = self.client.get('/')
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+
+class ListViewTest(TestCase):
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 
 
 class ItemModelTest(TestCase):
