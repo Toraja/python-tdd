816d128637755657f7675e64a576bd858b96f454
Toraja <toraja.blend.french@gmail.com>
2020-01-10 17:50:00 +0900

    Posting to resouce with list ID now adds item to the list

--- lists/views.py
+++ lists/views.py
@@ -20,3 +20,8 @@ class ListPageView(View):
         list_ = List.objects.get(id=list_id)
         items = Item.objects.filter(list=list_)
         return render(request, 'list.html', {'items': items})
+
+    def post(self, request, list_id):
+        list_to_add = List.objects.get(id=list_id)
+        Item.objects.create(text=request.POST['item_text'], list=list_to_add)
+        return redirect(f'/lists/{list_to_add.id}/')
