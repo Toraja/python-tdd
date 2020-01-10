afbe152c408b43dae533e2d91819730154e711e1
Toraja <toraja.blend.french@gmail.com>
2020-01-10 10:03:26 +0900

    Added List model and association

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     """Docstring for Item. """
 
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
