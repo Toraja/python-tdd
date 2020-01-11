048e0db657776464a56adbba3936e3ea6e7e47a2
Toraja <toraja.blend.french@gmail.com>
2020-01-11 21:24:43 +0900

    Refactored FT; use constant for id for inputbox

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -7,6 +7,7 @@ import os
 
 MAX_WAIT_SEC = 10
 INTERVAL_SEC = 0.5
+ID_INPUTBOX = 'id_new_item'
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -55,7 +56,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id(ID_INPUTBOX)
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item')
 
@@ -74,7 +75,7 @@ class NewVisitorTest(LiveServerTestCase):
         # There is still a text box invinting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id(ID_INPUTBOX)
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
@@ -92,7 +93,7 @@ class NewVisitorTest(LiveServerTestCase):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id(ID_INPUTBOX)
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         self.run_func_until_time_has_passed(MAX_WAIT_SEC,
@@ -118,7 +119,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis starts a new list by entering a new itme.
         # He is less interesting than Edith...
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id(ID_INPUTBOX)
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.run_func_until_time_has_passed(MAX_WAIT_SEC,
