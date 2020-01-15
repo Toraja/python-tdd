974d6cc51759b7c82f4655984fc36ddafdf47e7c
Toraja <toraja.blend.french@gmail.com>
2020-01-15 20:23:34 +0900

    Enhanced layout using Bootstrap

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -10,7 +10,7 @@ INTERVAL_SEC = 0.5
 ID_INPUTBOX = 'id_new_item'
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     debug = os.environ.get('debug') == '1'
 
